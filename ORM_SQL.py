15b11f52857d5b785b04a341f7a4401729fc0b7e
Ilya Shevelev <robotedu@yandex.ru>
2023-07-24 20:50:52 +0300

    Second version

--- ORM_SQL.py
+++ ORM_SQL.py
@@ -2,60 +2,12 @@ import sqlalchemy
 import psycopg2
 import json
 import os
+import datetime
+import models
+from models import Base, Publisher, Book, Shop, Stock, Sale
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
-from sqlalchemy import CheckConstraint
-
-Base = declarative_base()
-
-
-class Publisher(Base):
-    __tablename__ = "publisher"
-
-    id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(length=40), unique=True)
-
-
-class Book(Base):
-    __tablename__ = "book"
-
-    id = sq.Column(sq.Integer, primary_key=True)
-    title = sq.Column(sq.String(length=40), nullable=False)
-    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
-
-    publisher = relationship(Publisher, backref="books")
-
-
-class Shop(Base):
-    __tablename__ = "shop"
-
-    id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(length=40), unique=True)
-
-
-class Stock(Base):
-    __tablename__ = "stock"
-
-    id = sq.Column(sq.Integer, primary_key=True)
-    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
-    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
-    count = sq.Column(sq.Integer)
-
-    book = relationship(Book, backref="stocks")
-    shop = relationship(Shop, backref="stocks")
-
-
-class Sale(Base):
-    __tablename__ = "sale"
-
-    id = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Float)
-    date_sale = sq.Column(sq.Date)
-    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
-    count = sq.Column(sq.Integer)
-
-    stock = relationship(Stock, backref="sales")
-
+from sqlalchemy import CheckConstraint, or_
 
 def create_tables(engine):
     Base.metadata.drop_all(engine)
@@ -75,38 +27,13 @@ def read_json(file_path):
         session.add(model(id=record.get('pk'), **record.get('fields')))
     session.commit()
 
-def search_sale(publ_name,publ_id):
+def search_sale(publ_name = '', publ_id = 0):
     # запрос
     # название книги | название магазина, в котором была куплена эта книга | стоимость покупки | дата покупки
 
-    if publ_id != 0:
-        q=session.query(
-            Publisher.name, Shop.name, Sale.price, Sale.date_sale
-        ).join(
-            Book
-        ).join(
-            Stock
-        ).join(
-            Shop
-        ).join(
-            Sale
-        ).filter(
-            Publisher.id == publ_id
-        ).all()
-    else:
-        q=session.query(
-            Publisher.name, Shop.name, Sale.price, Sale.date_sale
-        ).join(
-            Book
-        ).join(
-            Stock
-        ).join(
-            Shop
-        ).join(
-            Sale
-        ).filter(
-            Publisher.name == publ_name
-        ).all()
+    q=session.query(Publisher.name, Shop.name, Sale.price, Sale.date_sale). \
+        join(Book).join(Stock).join(Shop).join(Sale). \
+        filter(or_(Publisher.id == publ_id, Publisher.name == publ_name)).all()
     for res in q:
         print(f'{res[0]} | {res[1]} | {res[2]} | {res[3]}')
 
---
+++ models.py
@@ -0,0 +1,52 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy import CheckConstraint
+
+Base = declarative_base()
+
+class Publisher(Base):
+    __tablename__ = "publisher"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True)
+
+
+class Book(Base):
+    __tablename__ = "book"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.String(length=40), nullable=False)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
+
+    publisher = relationship(Publisher, backref="books")
+
+
+class Shop(Base):
+    __tablename__ = "shop"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True)
+
+
+class Stock(Base):
+    __tablename__ = "stock"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
+    count = sq.Column(sq.Integer)
+
+    book = relationship(Book, backref="stocks")
+    shop = relationship(Shop, backref="stocks")
+
+
+class Sale(Base):
+    __tablename__ = "sale"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Float)
+    date_sale = sq.Column(sq.Date)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
+    count = sq.Column(sq.Integer)
+
+    stock = relationship(Stock, backref="sales")
